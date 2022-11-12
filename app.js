f8b0bf1581265ba3d825025b4b9344271d15d6c6
Elmigue10 <miguelvalbuena1026@gmail.com>
2022-11-12 14:37:16 -0500

    Oculus game v1

---
+++ app.js
@@ -0,0 +1,71 @@
+let count = 0;
+
+AFRAME.registerComponent('target', {
+  init: function(){
+    this.el.setAttribute("color", getRandomColor());
+    let bannerText = document.getElementById("banner-text");
+    bannerText.setAttribute("value", "WELCOME!");
+    bannerText.setAttribute("width", "20");
+    setTimeout(() => {
+      bannerText.setAttribute("value", "3");
+      bannerText.setAttribute("position","0 10 -4");
+    }, 1000);
+    setTimeout(() => {bannerText.setAttribute("value", "2");}, 2000);
+    setTimeout(() => {bannerText.setAttribute("value", "1");}, 3000); 
+    setTimeout(() => {
+      bannerText.setAttribute("value", "¡PLAY!");
+      bannerText.setAttribute("position","-1 10 -4");
+    }, 4000); 
+  }
+});
+
+AFRAME.registerComponent('trigger-check', {
+  init: function () {
+       
+    this.el.addEventListener('triggerdown', function (e) {
+        
+        controllershoot();
+        
+    });
+  },
+  
+  
+});
+
+AFRAME.registerComponent('collide-detect', {
+
+  init: function () {
+    let bulletEl = this.el;
+   
+    let debugtxt = document.querySelector('a-text');
+    let bannerText = document.getElementById("banner-text");
+
+    this.el.addEventListener('collide', function(e){
+      if(e.detail.body.el.className == 'target') {        
+        try{
+          
+          e.detail.body.el.parentNode.removeChild(e.detail.body.el);
+          count++;
+          debugtxt.setAttribute('value', 'COUNT: ' + count);
+          
+          if(count == 6){
+            bannerText.setAttribute("value", "YOU WIN!");
+          }
+        } catch (err){ }
+      } 
+
+      try {
+        bulletEl.parentNode.removeChild(e.detail.target.el);
+      }catch(err){}     
+    });
+  },
+});
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
---
+++ shoot.js
@@ -0,0 +1,55 @@
+var sceneEl = document.querySelector("a-scene");
+var cameraEl = document.getElementById("camera");
+var controllerEl = document.getElementById("rtcontroller");
+
+const shoot = () => {
+  const bullet = document.createElement("a-sphere");
+  let pos = cameraEl.getAttribute("position");
+  bullet.setAttribute("position", pos);
+  bullet.setAttribute("color", getRandomColor());
+
+  var direction = new THREE.Vector3();
+  cameraEl.object3D.getWorldDirection( direction );
+  bullet.setAttribute("velocity", direction.multiplyScalar(-10));
+  bullet.setAttribute("dynamic-body", {shape: 'box', mass: 5});
+  bullet.setAttribute("radius", 0.2);
+  bullet.setAttribute("collide-detect", null);
+
+  sceneEl.appendChild(bullet);
+};
+
+document.onkeydown = (event) => {
+  if (event.which == 32) {
+    shoot();
+  }
+};
+
+const controllershoot = () => {
+  const bullet = document.createElement("a-sphere");
+
+  var position = new THREE.Vector3();
+
+  controllerEl.object3D.getWorldPosition(position);
+  
+  bullet.setAttribute("position", position);
+
+  var direction = new THREE.Vector3();
+  controllerEl.object3D.getWorldDirection( direction );
+
+  bullet.setAttribute("velocity", direction.multiplyScalar(-20));
+  
+  bullet.setAttribute("dynamic-body", {shape: 'box', mass: 1});
+  bullet.setAttribute("radius", 0.2);
+  bullet.setAttribute("collide-detect", null);
+  
+  sceneEl.appendChild(bullet);
+};
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
